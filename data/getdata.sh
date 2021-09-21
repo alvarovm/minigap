10efa434a4f460aacfbe67263aad4328c6c52b0c
v4m4 <fray.gory@gmail.com>
2021-09-21 14:28:47 -0500

    adding externaldata download

---
+++ data/getdata.sh
@@ -0,0 +1,7 @@
+curl https://web.alcf.anl.gov/~vama/minigap/data/10Graphenes.extxyz.gz -o 10Graphenes.extxyz.gz
+curl https://web.alcf.anl.gov/~vama/minigap/data/C7O2H10.extxyz.gz -o C7O2H10.extxyz.gz
+curl https://web.alcf.anl.gov/~vama/minigap/data/ManyGraphenes.gz -o ManyGraphenes.gz
+curl https://web.alcf.anl.gov/~vama/minigap/data/QM9.xyz.tar.bz2 -o QM9.xyz.tar.bz2
+curl https://web.alcf.anl.gov/~vama/minigap/data/distorted_propanol.extxyz.gz -o distorted_propanol.extxyz.gz
+curl https://web.alcf.anl.gov/~vama/minigap/data/element_default_values.txt -o element_default_values.txt
+
